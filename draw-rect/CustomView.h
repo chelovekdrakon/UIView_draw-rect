d1d896f27622dafd783f53689d4912f24964408e
Fiodar Morau <chelovekdrakon@gmail.com>
2019-07-05 00:12:45 +0300

    init project

---
+++ draw-rect/CustomView.h
@@ -0,0 +1,13 @@
+//
+//  CustomView.h
+//  draw-rect
+//
+//  Created by Фёдор Морев on 7/5/19.
+//  Copyright © 2019 Фёдор Морев. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomView : UIView
+
+@end
---
+++ draw-rect/CustomView.m
@@ -0,0 +1,21 @@
+//
+//  CustomView.m
+//  draw-rect
+//
+//  Created by Фёдор Морев on 7/5/19.
+//  Copyright © 2019 Фёдор Морев. All rights reserved.
+//
+
+#import "CustomView.h"
+
+@implementation CustomView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
