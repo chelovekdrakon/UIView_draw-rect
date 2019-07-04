9fe87dc13342c8559031348992f5e2bd6f82d803
Fiodar Morau <chelovekdrakon@gmail.com>
2019-07-05 00:10:53 +0300

    Initial Commit

---
+++ draw-rect/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  draw-rect
+//
+//  Created by Фёдор Морев on 7/5/19.
+//  Copyright © 2019 Фёдор Морев. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ draw-rect/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  draw-rect
+//
+//  Created by Фёдор Морев on 7/5/19.
+//  Copyright © 2019 Фёдор Морев. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
