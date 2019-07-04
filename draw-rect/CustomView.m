60c5119773223cda58b801f3ee66a4f45ebeb9a0
Fiodar Morau <chelovekdrakon@gmail.com>
2019-07-05 00:54:03 +0300

    Some drawings

--- draw-rect/CustomView.m
+++ draw-rect/CustomView.m
@@ -16,12 +16,32 @@
     CGContextSetFillColorWithColor(context, [UIColor blackColor].CGColor);
     CGContextFillRect(context, rect);
     
-    CGRect square1 = CGRectMake(100, 100, 100, 100);
+    CGRect square1 = CGRectMake(150, 150, 100, 100);
     
     CGContextAddRect(context, square1);
     
     CGContextSetFillColorWithColor(context, [UIColor yellowColor].CGColor);
     CGContextFillPath(context);
+    
+    CGContextMoveToPoint(context, CGRectGetMaxX(square1), CGRectGetMaxY(square1));
+    CGContextAddLineToPoint(context, CGRectGetMaxX(square1) + 100, CGRectGetMaxY(square1) + 100);
+    
+    CGContextSetStrokeColorWithColor(context, [UIColor yellowColor].CGColor);
+    CGContextStrokePath(context);
+    
+    CGContextMoveToPoint(context, CGRectGetMinX(square1), CGRectGetMinX(square1));
+    CGContextAddLineToPoint(context, CGRectGetMinX(square1) - 100, CGRectGetMinY(square1) - 100);
+    
+    CGContextSetLineWidth(context, 3.f);
+    CGContextSetLineCap(context, kCGLineCapRound);
+    
+    CGContextSetStrokeColorWithColor(context, [UIColor yellowColor].CGColor);
+    CGContextStrokePath(context);
+    
+//    CGContextAddArcToPoint(context, CGRectGetMinX(square1) - 100, CGRectGetMinY(square1) - 100, CGRectGetMaxX(square1) + 100, CGRectGetMaxY(square1) + 100, M_PI);
+//    CGContextAddQuadCurveToPoint(context, CGRectGetMinX(square1) - 100, CGRectGetMinY(square1) - 100, CGRectGetMaxX(square1) + 100, CGRectGetMaxY(square1) + 100);
+    CGContextAddArc(context, 50, 50, 100, M_PI, 0, YES);
+    CGContextStrokePath(context);
 }
 
 @end
